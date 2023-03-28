4b24b03b19cc7fc38c1b1e30bc61c20a20ab7d4e
Sankar-sj14 <108957439+Sankar-sj14@users.noreply.github.com>
2023-03-28 14:58:35 +0530

    Add files via upload

---
+++ LAB6/Lab6package.java
@@ -0,0 +1,34 @@
+package com.artworkselling;
+
+public class Artwork implements Saleable {
+
+    private String artist;
+    private String title;
+    private double price;
+
+    public Artwork(String artist, String title, double price) {
+        this.artist = artist;
+        this.title = title;
+        this.price = price;
+    }
+
+    public void displayArtwork() {
+        System.out.println("Artist: " + artist);
+        System.out.println("Title: " + title);
+        System.out.println("Price: " + price);
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
+
+interface Saleable {
+    double getPrice();
+}
+
+
